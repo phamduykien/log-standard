4e44971011f60e1303ca1e0b94e1494b68d68d17
phamduykien <phamduykien@gmail.com>
2024-03-20 17:20:16 +0700

    First add

---
+++ MISA.LogStandard/MISA.LogStandard.Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿using NLog.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//Thêm nlog để ghi log đến logstash
+builder.Services.AddLogging(loggingBuilder =>
+{
+    
+    loggingBuilder.ClearProviders();    
+    loggingBuilder.AddNLog("E:\\Project\\LogSandard\\MISA.LogStandard\\MISA.LogStandard.Sample\\bin\\Debug\\net8.0\\Config\\nlog.config");
+});
+
+//builder.Logging.AddNLog("Config/nlog.xml");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
