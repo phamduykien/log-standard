015d6860216481876eb12276cccb4c4829d9f2ff
phamduykien <phamduykien@gmail.com>
2024-03-25 16:31:13 +0700

    aa

--- MISA.LogStandard/MISA.LogStandard.Sample/Controllers/WeatherForecastController.cs
+++ MISA.LogStandard/MISA.LogStandard.Sample/Controllers/WeatherForecastController.cs
@@ -23,6 +23,10 @@ namespace MISA.LogStandard.Sample.Controllers
         {
             _logger.LogInformation($"Get weather forecast {DateTime.Now.ToLocalTime()}");
             _logger.LogError("Error");
+
+            HttpClient httpClient = new HttpClient();
+           var x= httpClient.PostAsync("http://43.199.2.247:8080",new StringContent("Message from app")).GetAwaiter().GetResult();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
